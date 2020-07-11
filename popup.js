9038d07173275dc5dd6c42138d652e158b916ae1
Twixtaxe <46041463+Twixtaxe@users.noreply.github.com>
2020-07-11 11:12:28 -0300

    Finalizando  a pagina Inicial
    Concluido os scripts da pagina inicial

--- popup.js
+++ popup.js
@@ -11,19 +11,24 @@ let h3 = body.querySelectorAll(".widget-header");
 let links = body.querySelectorAll("a");
 let paragrafo = body.querySelectorAll("p");
 let span = body.querySelectorAll(".external-link");
-
-
+let settings = body.querySelector(".header-right");
+let profileMenuDropdown = body.querySelector(".header-profile-dropdown");
 let add = body.querySelectorAll(".Lightbox_AddEdit");
 let text = body.querySelectorAll(".text");
 let textRead = body.querySelectorAll(".textReadability");
 let spaceIt = body.querySelectorAll(".spaceit");
 let widgetEpisodeVideo = body.querySelector("#widget-episode-video");
 let widgetSlide = body.querySelectorAll(".widget-slide-outer");
+let menuLeft = body.querySelector("#menu_left").querySelectorAll("ul");
+let ulMenuDropDown =  profileMenuDropdown.querySelector("ul");
 
+let teste = body.querySelector("#contentWrapper");
 
 let rightColumn =  body.querySelector(".right-column");
 
-let list = rightColumn.querySelectorAll("ul")
+let list = rightColumn.querySelectorAll("ul");
+
+let footer =  body.querySelector("#footer-block");
 
 body.style.background = "#242526";
 
@@ -37,6 +42,10 @@ for(var i = 0; i < 3; i++){
 
     borderHeader[i].style.display = "none";
 }
+
+settings.style.color = "#242526";
+
+settings.querySelector("i").style.color = "#000";
 
 userIconArea.style.backgroundColor = "#3A3B3A";
 userIconArea.style.borderRadius = "5px";
@@ -79,4 +88,36 @@ list[i].style.backgroundColor = "#242526"
 for(var i = 0; i < add.length; i++){
     add[i].style.backgroundColor = "#242526"
     add[i].style.textDecoration ="none"
+}
+
+let menuUls = menuLeft[0].querySelectorAll("ul");
+let menuLis = menuUls[0].querySelectorAll("li");
+
+
+let menuListeste = [];
+
+for( var i = 0; i < menuUls.length; i++){
+
+    var index = menuUls[i].querySelectorAll("li");
+    menuListeste.push(index);
+   
+}
+
+    for(var i = 0; i < menuListeste.length; i++ ){
+        
+        var temp = menuListeste[i];
+     
+        
+        for(var index = 0; index < temp.length; index++){
+
+            temp[index].querySelector("a").style.backgroundColor = "#242526";
+        }
     }
+    var lisMenu = ulMenuDropDown.querySelectorAll("li");
+
+    console.log(lisMenu)
+  for(var i = 0; i < lisMenu.length; i++){
+    lisMenu[i].querySelector("a").style.backgroundColor = "#242526";
+  }
+
+  footer.style.backgroundColor = "#242526"
