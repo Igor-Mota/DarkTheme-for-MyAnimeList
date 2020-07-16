16a8cf857781fc71e7c6367146bb90c8f074310f
Twixtaxe <46041463+Twixtaxe@users.noreply.github.com>
2020-07-16 16:33:59 -0300

    alterando menu principal
    aplicando estilos e funcionalidade de hover no menu principal

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var PageStyles = document.createElement("style");
+let PageStyles = document.createElement("style");
 
 let preto = "#242526";
 let pretoC ="#3a3b3c";
@@ -22,12 +22,43 @@ PageStyles.innerHTML += ` .external-link{background-color:${preto} !important}`;
 PageStyles.innerHTML += ` .widget-slide-outer{width:100% !important; overflow:"inherit" !important}`;
 PageStyles.innerHTML += `  ul{background-color:${preto} !important}`;
 PageStyles.innerHTML += ` .Lightbox_AddEdit{background-color:${preto} !important}`;
-PageStyles.innerHTML += ` .non-link{color:${pretoC} !important}`;
+PageStyles.innerHTML += ` .non-link{color: #fff !important}`;
 PageStyles.innerHTML += `  #footer-block{background-color:${preto} !important}`;
-PageStyles.innerHTML += ` .wider li{background-color:${preto} !important}`;
-PageStyles.innerHTML += `  #menu{width:100% !important}`;
+PageStyles.innerHTML += ` .wider{background-color:${preto} !important}`;
+PageStyles.innerHTML += `  #menu{background-color:${preto} !important ; border 1px solid #c4c4c4 !important; width:100% !important}`;
 PageStyles.innerHTML += `  #myanimelist{width:98% !important}`;
 PageStyles.innerHTML += `  #widget-episode-video{width:100% !important}`;
+PageStyles.innerHTML += `  h2{color:#fff !important}`;
+
 
 document.head.appendChild(PageStyles);
+
+try{
+    let nav = document.querySelector("#nav");
+    let lisNav = nav.querySelectorAll("li");
+
+    for(var i = 0; i < lisNav.length; i++){
+        lisNav[i].addEventListener("mousemove", brilho);
+        lisNav[i].addEventListener("mouseout", brilhoFora);
+    }
+    nav = nav.querySelectorAll("ul");
+    for(var i = 0; i < nav.length; i++){
+        nav[i].style.borderRadius = "15px"
+        var temp = nav[i].querySelectorAll("li");
+        for(var c = 0; c < temp.length; c++ ){
+            temp[c].querySelector("a").style.backgroundColor = preto;
+            temp[c].querySelector("a").addEventListener("mousemove", brilho);
+            temp[c].querySelector("a").addEventListener("mouseout", brilhoFora);
+     
+        }    
+    }
+}catch(e){}
+
+function brilho(e){
+    e.target.style.backgroundColor = pretoC;
+ 
+}
+function brilhoFora(e){
+    e.target.style.backgroundColor = preto;
+}
 
